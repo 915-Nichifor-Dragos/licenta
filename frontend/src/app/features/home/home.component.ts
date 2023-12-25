e98390059e795fa13c2ba86dd15b413faff76a76
dragos.nichifor <dragos.nichifor@pitechplus.com>
2023-12-25 13:39:54 +0200

    💄 Redesigned home page

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input, inject } from '@angular/core';
+
 import { AuthService } from '../auth/auth.service';
+
 import { UserClaim } from 'src/app/features/auth/auth.model';
+
+import { AppPageHeaderComponent } from 'src/app/shared/page-header/page-header.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
+  imports: [AppPageHeaderComponent],
 })
 export class HomeComponent {
   authService = inject(AuthService);
