00a5b443cc8c1da079c62a99ba7c763507614523
dragos.nichifor <dragos.nichifor@pitechplus.com>
2023-12-25 15:13:13 +0200

    💄 Redesign the entire register page

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -7,15 +7,17 @@ import {
 } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
 import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { AuthService } from '../auth.service';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
 
 import { AppPageHeaderComponent } from 'src/app/shared/page-header/page-header.component';
 
@@ -35,6 +37,7 @@ import { AppPageHeaderComponent } from 'src/app/shared/page-header/page-header.c
     RouterModule,
     MatButtonModule,
     AppPageHeaderComponent,
+    MatStepperModule,
   ],
 })
 export class RegisterComponent {
@@ -43,7 +46,29 @@ export class RegisterComponent {
   snackBar = inject(MatSnackBar);
   router = inject(Router);
 
-  userForm = this.formBuilder.group(
+  userDetailsFormGroup = this.formBuilder.group({
+    firstName: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(100)],
+    ],
+    lastName: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(100)],
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]{3,}\.[A-Za-z]{2,4}$/
+        ),
+      ],
+    ],
+    birthDate: ['', [Validators.required]],
+    gender: ['', [Validators.required]],
+  });
+
+  userCredentialsFormGroup = this.formBuilder.group(
     {
       username: [
         '',
@@ -66,83 +91,175 @@ export class RegisterComponent {
         ],
       ],
       confirmPassword: ['', [Validators.required]],
-      firstName: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(100),
-        ],
-      ],
-      lastName: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(100),
-        ],
-      ],
-      email: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern(
-            /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]{3,}\.[A-Za-z]{2,4}$/
-          ),
-        ],
-      ],
-      birthDate: ['', [Validators.required]],
-      gender: ['', [Validators.required]],
-      address: ['', Validators.maxLength(100)],
-      bio: ['', Validators.maxLength(1000)],
-      profilePicture: [
-        '',
-        Validators.pattern('([a-zA-Z0-9s_\\.-:])+(.jpg|.png|.jpeg)$'),
-      ],
     },
     { validator: this.passwordConfirmationValidator }
   );
 
-  createAccount() {
-    if (this.userForm.valid) {
-      const usernameControl = this.userForm.get('username')!;
-      const passwordControl = this.userForm.get('password')!;
-      const firstNameControl = this.userForm.get('firstName')!;
-      const lastNameControl = this.userForm.get('lastName')!;
-      const emailControl = this.userForm.get('email')!;
-      const birthDateControl = this.userForm.get('birthDate')!;
-      const genderControl = this.userForm.get('gender')!;
-      const addressControl = this.userForm.get('address')!;
-      const bioControl = this.userForm.get('bio')!;
-      //const imageUrlControl = this.userForm.get('imageUrl')!;
+  userAdditionalInfoFormGroup = this.formBuilder.group({
+    address: ['', Validators.maxLength(100)],
+    bio: ['', Validators.maxLength(1000)],
+    profilePicture: [
+      '',
+      Validators.pattern('([a-zA-Z0-9s_\\.-:])+(.jpg|.png|.jpeg)$'),
+    ],
+  });
 
-      const userData = {
-        username: usernameControl.value,
-        password: passwordControl.value,
-        firstName: firstNameControl.value,
-        lastName: lastNameControl.value,
-        email: emailControl.value,
-        birthDate: birthDateControl.value.toISOString(),
-        gender: parseInt(genderControl.value),
-        address: addressControl?.value,
-        bio: bioControl?.value,
-        //imageUrl: imageUrlControl?.value
-      };
+  getFirstNameErrorMessage() {
+    const firstNameControl = this.userDetailsFormGroup.get('firstName');
 
-      this.authService.register(userData).subscribe(
-        (response) => {
-          this.snackBar.open('Register was successful', 'Close', {
-            duration: 2000,
-          });
-          this.router.navigate(['/login']);
-        },
-        (error) => {
-          this.snackBar.open('Register failed', 'Close', {
-            duration: 2000,
-          });
-        }
-      );
+    if (!firstNameControl) {
+      return;
     }
+
+    if (firstNameControl.hasError('required')) {
+      return 'You must enter a value for first name';
+    }
+
+    if (firstNameControl.hasError('minlength')) {
+      return 'The first name must be at least 2 characters';
+    }
+
+    if (firstNameControl.hasError('maxlength')) {
+      return 'The first name must be at mose 100 characters';
+    }
+
+    return '';
+  }
+
+  getLastNameErrorMessage() {
+    const lastNameControl = this.userDetailsFormGroup.get('lastName');
+
+    if (!lastNameControl) {
+      return;
+    }
+
+    if (lastNameControl.hasError('required')) {
+      return 'You must enter a value for first name';
+    }
+
+    if (lastNameControl.hasError('minlength')) {
+      return 'The last name must be at least 2 characters';
+    }
+
+    if (lastNameControl.hasError('maxlength')) {
+      return 'The last name must be at mose 100 characters';
+    }
+
+    return '';
+  }
+
+  getEmailErrorMessage() {
+    const emailControl = this.userDetailsFormGroup.get('email');
+
+    if (!emailControl) {
+      return;
+    }
+
+    if (emailControl.hasError('required')) {
+      return 'You must enter a value for email';
+    }
+
+    if (emailControl.hasError('pattern')) {
+      return 'Email must include @ and a domain. Characters <>/(),. are not allowed"';
+    }
+
+    return '';
+  }
+
+  getBirthDateErrorMessage() {
+    const birthDateControl = this.userDetailsFormGroup.get('birthDate');
+
+    if (!birthDateControl) {
+      return;
+    }
+
+    if (birthDateControl.hasError('required')) {
+      return 'You must enter a value for birth date';
+    }
+
+    return '';
+  }
+
+  getGenderErrorMessage() {
+    const genderControl = this.userDetailsFormGroup.get('gender');
+
+    if (!genderControl) {
+      return;
+    }
+
+    if (genderControl.hasError('required')) {
+      return 'You must enter a value for gender';
+    }
+
+    return '';
+  }
+
+  getUsernameErrorMessage() {
+    const usernameControl = this.userCredentialsFormGroup.get('username');
+
+    if (!usernameControl) {
+      return;
+    }
+
+    if (usernameControl.hasError('required')) {
+      return 'You must enter a value for username';
+    }
+
+    if (usernameControl.hasError('minlength')) {
+      return 'The username must be at least 4 characters';
+    }
+
+    if (usernameControl.hasError('maxlength')) {
+      return 'The username must be at mose 30 characters';
+    }
+
+    if (usernameControl.hasError('pattern')) {
+      return 'The username must only contain alphanumeric characters';
+    }
+
+    return '';
+  }
+
+  getPasswordErrorMessage() {
+    const passwordControl = this.userCredentialsFormGroup.get('password');
+
+    if (!passwordControl) {
+      return;
+    }
+
+    if (passwordControl.hasError('required')) {
+      return 'You must enter a value for username';
+    }
+
+    if (passwordControl.hasError('minlength')) {
+      return 'The username must be at least 8 characters';
+    }
+
+    if (passwordControl.hasError('maxlength')) {
+      return 'The username must be at mose 30 characters';
+    }
+
+    if (passwordControl.hasError('pattern')) {
+      return `No white spaces are allowed. Must have at least 1 lowercase letter,
+        digit and special character`;
+    }
+
+    return '';
+  }
+
+  getConfirmPasswordErrorMessage() {
+    const confirmPasswordControl =
+      this.userCredentialsFormGroup.get('confirmPassword');
+
+    if (!confirmPasswordControl) {
+      return;
+    }
+
+    if (confirmPasswordControl.hasError('passwordMismatch')) {
+      return 'Must be the same as the password';
+    }
+
+    return '';
   }
 
   passwordConfirmationValidator(formGroup: FormGroup) {
@@ -157,6 +274,99 @@ export class RegisterComponent {
       confirmPassword.setErrors({ passwordMismatch: true });
     } else {
       confirmPassword?.setErrors(null);
+    }
+  }
+
+  getAddressErrorMessage() {
+    const addressControl = this.userAdditionalInfoFormGroup.get('address');
+
+    if (!addressControl) {
+      return;
+    }
+
+    if (addressControl.hasError('maxlength')) {
+      return 'The address can be at most 100 characters';
+    }
+
+    return '';
+  }
+
+  getBioErrorMessage() {
+    const bioControl = this.userAdditionalInfoFormGroup.get('bio');
+
+    if (!bioControl) {
+      return;
+    }
+
+    if (bioControl.hasError('maxlength')) {
+      return 'The bio can be at most 1000 characters';
+    }
+
+    return '';
+  }
+
+  getProfilePictureErrorMessage() {
+    const profilePictureControl =
+      this.userCredentialsFormGroup.get('profilePicture');
+
+    if (!profilePictureControl) {
+      return;
+    }
+
+    if (profilePictureControl.hasError('pattern')) {
+      return 'Must be .jpg, .png, .jpeg and under 10MB';
+    }
+
+    return '';
+  }
+
+  createAccount() {
+    if (
+      this.userAdditionalInfoFormGroup.valid &&
+      this.userCredentialsFormGroup.valid &&
+      this.userDetailsFormGroup.valid
+    ) {
+      const usernameControl = this.userCredentialsFormGroup.get('username')!;
+      const passwordControl = this.userCredentialsFormGroup.get('password')!;
+      const firstNameControl = this.userDetailsFormGroup.get('firstName')!;
+      const lastNameControl = this.userDetailsFormGroup.get('lastName')!;
+      const emailControl = this.userDetailsFormGroup.get('email')!;
+      const birthDateControl = this.userDetailsFormGroup.get('birthDate')!;
+      const genderControl = this.userDetailsFormGroup.get('gender')!;
+      const addressControl = this.userAdditionalInfoFormGroup.get('address')!;
+      const bioControl = this.userAdditionalInfoFormGroup.get('bio')!;
+      //const imageUrlControl = this.userAdditionalInfoFormGroup.get('imageUrl')!;
+
+      if (!birthDateControl.value || !genderControl.value) {
+        return;
+      }
+
+      const userData = {
+        username: usernameControl.value,
+        password: passwordControl.value,
+        firstName: firstNameControl.value,
+        lastName: lastNameControl.value,
+        email: emailControl.value,
+        birthDate: birthDateControl.value.toString(),
+        gender: parseInt(genderControl.value),
+        address: addressControl?.value,
+        bio: bioControl?.value,
+        //imageUrl: imageUrlControl?.value
+      };
+
+      this.authService.register(userData).subscribe(
+        () => {
+          this.snackBar.open('Register was successful', 'Close', {
+            duration: 2000,
+          });
+          this.router.navigate(['/login']);
+        },
+        () => {
+          this.snackBar.open('Register failed', 'Close', {
+            duration: 2000,
+          });
+        }
+      );
     }
   }
 }
